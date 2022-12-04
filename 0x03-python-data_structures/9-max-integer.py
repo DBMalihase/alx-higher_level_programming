35a28726fa1a3bef12b37e147f89049a97c09898
DBMalihase <dennismalihase@gmail.com>
2022-12-04 07:48:27 -0800

    Python Data structure

--- 0x03-python-data_structures/9-max-integer.py
+++ 0x03-python-data_structures/9-max-integer.py
@@ -3,9 +3,5 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    else:
-        max_int = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > max_int:
-                max_int = my_list[i]
-                return (max_int)
+    my_list.sort()
+    return (my_list[-1])
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -1,2 +1,1 @@
 Python data structure project
-
