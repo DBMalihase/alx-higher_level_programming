d3a251d0132944a93968c5b708e418e3282b2e62
DBMalihase <dennismalihase@gmail.com>
2022-12-04 07:42:15 -0800

    Python Data structure

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new_list = []
+    list_len = len(my_list)
+    if list_len == 0:
+        return None
 
-    for i in my_list:
-        if i % 2 == 0:
-            new_list.append(True)
+    bool_list = []
+    for i in range(list_len):
+        if my_list[i] % 2 == 0:
+            bool_list.append(True)
         else:
-            new_list.append(False)
-            return (new_list)
+            bool_list.append(False)
+            return (bool_list)
