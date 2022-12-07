c20886e30e2b518d500de91c56de11e73d0e4233
DBMalihase <dennismalihase@gmail.com>
2022-12-07 13:35:36 -0800

    More data structure

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-simply_delete = __import__('8-simple_delete').simple_delete
+simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = \
         __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
