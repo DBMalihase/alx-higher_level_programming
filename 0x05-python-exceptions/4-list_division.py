3df63403d35915470e07eaeff83387938e8529cd
DBMalihase <dennismalihase@gmail.com>
2022-12-19 08:39:41 -0800

    Python exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     """
     Takes two list and creates a new list with result of division operation
     """
-    i - 0
+    i = 0
     new_list = []
     result = 0
     for i in range(0, list_lenth):
